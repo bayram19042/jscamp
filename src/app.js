ab9ab26785e92a67a507e73f8045dfaa445f2240
bayram19042 <bayram19042@gmail.com>
2021-10-19 00:02:18 +0300

    18.10.2021

---
+++ src/app.js
@@ -0,0 +1,18 @@
+console.log("merhaba kodlama.io")
+
+
+let dolarBugun  = 9.30
+
+var dolarDun = 9.20
+{
+    var dolarDun = 9.10
+}
+console.log(dolarDun)
+const euroDun = 11
+
+console.log(euroDun)
+let users = ["konut kredisi","emlak konut kredisi","kamu konut kredisi",["a","b","c"]]
+for (let i = 0; i < users.length; i++) {
+    console.log(users[i])
+    
+}
