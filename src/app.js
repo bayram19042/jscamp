ad8128192270e4dcb3ff610e064f7c038bc7dc5e
bayram19042 <bayram19042@gmail.com>
2021-10-19 00:13:03 +0300

    https://github.com/bayram19042/jscamp.git

--- src/app.js
+++ src/app.js
@@ -13,6 +13,6 @@ const euroDun = 11
 console.log(euroDun)
 let users = ["konut kredisi","emlak konut kredisi","kamu konut kredisi",["a","b","c"]]
 for (let i = 0; i < users.length; i++) {
-    console.log(users[i])
+    console.log(users[i]);
     
 }
